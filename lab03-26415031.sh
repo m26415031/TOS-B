4b8ce5cf79c1512cfaeeac93d3b339184f4304ed
Martina Marcelline <m26415031@john.petra.ac.id>
2016-09-15 09:47:18 +0700

    curl klikbca

---
+++ lab03-26415031.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+jual= `curl -s http://www.klikbca.com/ | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f2`
+beli= `curl -s http://www.klikbca.com/ | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f3`
+echo "Harga jual: $jual"
+echo "Harga beli: $beli"
+
+
