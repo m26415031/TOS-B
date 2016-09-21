28e1c04749f9c918714f27038a4fd5ddc6575a10
Martina Marcelline <m26415031@john.petra.ac.id>
2016-09-21 23:06:56 +0700

    test

--- lab03-26415031.sh
+++ lab03-26415031.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-jual= `curl -s http://www.klikbca.com/ | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f2`
-beli= `curl -s http://www.klikbca.com/ | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f3`
-echo "Harga jual: $jual"
-echo "Harga beli: $beli"
-
+jual=`curl -s http://www.klikbca.com/ | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f2`
+beli=`curl -s http://www.klikbca.com/ | grep USD -A2 | cut -d">" -f2 | cut -d"<" -f1 | xargs | cut -d" " -f3`
+echo "Harga jual: "$jual
+echo "Harga beli: "$beli
 
